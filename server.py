a0490b81c86d88636fee5030ca75db0422540953
jorasingh <87735849+jorasingh@users.noreply.github.com>
2022-01-21 18:45:34 -0800

    modified

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+import js2py
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+# # this will translate example.js to example.py
+# js2py.translate_file('static/sketch.js', 'sketch.py')
+# # example.py can be now imported and used!
 
 
 @app.route('/')
